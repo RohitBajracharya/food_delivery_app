999541c9cbd16cde39a1919274c008e74268d0aa
RohitBajra <104704874+rohitnewgit@users.noreply.github.com>
2023-09-05 19:50:13 +0545

    Slider section loaded from api

--- lib/pages/home/food_page_body.dart
+++ lib/pages/home/food_page_body.dart
@@ -1,6 +1,7 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/data/controllers/popular_product_controller.dart';
+import 'package:food_delivery_app/utils/app_constants.dart';
 import 'package:food_delivery_app/utils/colors.dart';
 import 'package:food_delivery_app/widgets/app_column.dart';
 import 'package:food_delivery_app/widgets/big_text.dart';
@@ -8,6 +9,7 @@ import 'package:food_delivery_app/widgets/icon_and_text.dart';
 import 'package:food_delivery_app/widgets/small_text.dart';
 import 'package:get/get.dart';
 
+import '../../models/products_model.dart';
 import '../../utils/dimension.dart';
 
 class FoodPageBody extends StatefulWidget {
@@ -46,17 +48,22 @@ class _FoodPageBodyState extends State<FoodPageBody> {
         //slider section
         GetBuilder<PopularProductController>(
           builder: (popularProducts) {
-            return Container(
-              // color: Colors.redAccent,
-              height: Dimensions.pageView,
-              child: PageView.builder(
-                controller: pageController,
-                itemCount: popularProducts.popularProductList.length,
-                itemBuilder: (context, index) {
-                  return _buildPageItem(index);
-                },
-              ),
-            );
+            return popularProducts.isLoaded
+                ? Container(
+                    // color: Colors.redAccent,
+                    height: Dimensions.pageView,
+                    child: PageView.builder(
+                      controller: pageController,
+                      itemCount: popularProducts.popularProductList.length,
+                      itemBuilder: (context, index) {
+                        return _buildPageItem(
+                            index, popularProducts.popularProductList[index]);
+                      },
+                    ),
+                  )
+                : const CircularProgressIndicator(
+                    color: AppColors.mainColor,
+                  );
           },
         ),
         //dots indicator
@@ -64,7 +71,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
           builder: (popularProducts) {
             var length = popularProducts.popularProductList.length;
             return DotsIndicator(
-              dotsCount: length <= 0 ? 5 : length,
+              dotsCount: length <= 0 ? 1 : length,
               position: _currPageValue,
               decorator: const DotsDecorator(
                 activeColor: AppColors.mainColor,
@@ -186,7 +193,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   // method that return slider content
-  Widget _buildPageItem(int index) {
+  Widget _buildPageItem(int index, ProductModel popularProduct) {
     Matrix4 matrix = Matrix4.identity();
     if (index == _currPageValue.floor()) {
       var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
@@ -227,8 +234,9 @@ class _FoodPageBodyState extends State<FoodPageBody> {
               color: index.isEven
                   ? const Color(0xFF69c5df)
                   : const Color(0xFF9294cc),
-              image: const DecorationImage(
-                image: AssetImage("assets/images/pancake.jpg"),
+              image: DecorationImage(
+                image: NetworkImage(
+                    "${AppConstants.BASE_URL}/uploads/${popularProduct.img!}"),
                 fit: BoxFit.cover,
               ),
             ),
@@ -269,7 +277,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                   left: Dimensions.width15,
                   right: Dimensions.width15,
                 ),
-                child: const AppColumn(text: "Pancake with Honey"),
+                child: AppColumn(text: popularProduct.name!),
               ),
             ),
           ),
